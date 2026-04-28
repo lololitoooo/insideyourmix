1355b33ea09913e319635b203d6b504f4e7efed8
Loïc Lelouard <lolito@Host-003.lan>
2026-04-28 20:24:45 +0200

    fix indentation

--- app.py
+++ app.py
@@ -623,5 +623,5 @@ dims.forEach((d, i) => {
 def how_it_works():
     return HOW_IT_WORKS_HTML
 if __name__ == "__main__":
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
